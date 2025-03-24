1781fd941e5e4ab283b208bf8413467a0949457a
nevin <nevinsunny04@gmail.com>
2025-03-24 11:26:19 +0530

    Misc - Connecting to DB using Mongoose

--- NodeJs/Natours_API/natour_starting/server.js
+++ NodeJs/Natours_API/natour_starting/server.js
@@ -1,7 +1,14 @@
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 dotenv.config({path: './config.env'})
 const app = require('./app')
 //console.log(process.env)
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+mongoose.connect(DB, {}).then(con => {
+        console.log(con.connections)
+        console.log('DB connection successful')
+    })
 
 //Starting server
 const port = process.env.PORT
